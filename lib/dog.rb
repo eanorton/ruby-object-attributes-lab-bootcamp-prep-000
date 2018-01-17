8aaffbef5c24eee8d9bb5c0f4c0194e25975cc7d
Edward Norton <edwardnorton@icloud.com>
2018-01-17 21:54:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,3 @@
 class Dog 
-  def name=fido 
-    @name = fido 
-  end
-  
-  def name
-    @fido  
-  end
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end 
-end
+  def name=(dog_name)
+    
